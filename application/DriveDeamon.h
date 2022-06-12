f75dbcd277de47f7f6e9ef6af40e6af59815015b
schwemmdx <sven.prigann@web.de>
2022-06-12 19:34:58 +0200

    add DriveDeamon.cpp/h

---
+++ application/DriveDeamon.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+class DriveDeamon
+{
+    public:
+    DriveDeamon(std::stirng logical);
+    
+
+    private:
+    std::string logicalDrive;
+    std::string mntPoint;
+
+
+}
