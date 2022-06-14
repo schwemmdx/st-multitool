1a6c9f19be64746f4fbe42ee91c1c3f7ea6bd5a0
schwemmdx <sven.prigann@web.de>
2022-06-14 17:01:42 +0200

    created c/h files for eventhandler task

---
+++ framework/event_handler.c
@@ -0,0 +1,4 @@
+
+#include "event_handler.h"
+
+void fk_EvntHandlerTsk(void* param)
---
+++ framework/event_handler.h
@@ -0,0 +1,8 @@
+#ifndef _EVENT_HANDLER_H_
+#define  EVENT_HANDLER_H_
+
+
+void fk_EvntHandlerTsk(void*);
+
+
+#endif /* EVENT_HANDLER_H_*/
