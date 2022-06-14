76d213bc6538babe3b99c8721af492e58e596fb3
schwemmdx <sven.prigann@web.de>
2022-06-14 17:11:16 +0200

    implemented skeleton for eventhalder task

--- framework/event_handler.c
+++ framework/event_handler.c
@@ -1,4 +1,21 @@
 
 #include "event_handler.h"
 
+
+#include "FreeRTOS.h"
+#include "task.h"
+
 void fk_EvntHandlerTsk(void* param)
+{
+
+    fk_EvntHandler_t* pEvnt = (fk_EvntHandler_t*) param;
+
+    while(pEvnt->status == EVNT_H_RUNNING)
+    {
+
+        // Do shit 
+
+        
+        vTaskDelay(500);
+    }
+}
--- framework/event_handler.h
+++ framework/event_handler.h
@@ -1,6 +1,17 @@
 #ifndef _EVENT_HANDLER_H_
 #define  EVENT_HANDLER_H_
 
+#define EVNT_H_RUNNING 0
+#define EVNT_H_PAUSE 1
+#define EVNT_H_RESTART 2 
+
+typedef struct
+{
+    uint8_t status;
+    
+}fk_EvntHandler_t;
+
+void fk_EventHandlerInit(fk_EvntHandler_t* pEvnt);
 
 void fk_EvntHandlerTsk(void*);
 
